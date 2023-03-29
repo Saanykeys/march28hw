b2d82c42f28a68e1ff2527b7c04757511aa95b41
Saanykeys <125324312+Saanykeys@users.noreply.github.com>
2023-03-29 10:13:32 -0400

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,11 @@
+ //Exercise 1: Write a function called toCelcius that takes in a number representing 
+    //temperature in Fahrenheit and converts the number into Celcius.
+
+  //  Ex 1. toCelcius(32) // 0
+   // Ex 2. toCelcius(212) // 100
+ //   Ex 3. toCelcius(50) // 10
+*/
+//console.log('This is the homework!');
+
+
+  (°F − 32) × 5/9 = °C
